950ef172ad873352384f52d57fe51e7c48829e9e
wangzxc <15232514223@163.com>
2016-12-21 20:59:28 +0800

    first commit

---
+++ recycler的分割线.java
@@ -0,0 +1,7 @@
+recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+                super.getItemOffsets(outRect, view, parent, state);
+                outRect.set(0, 10, 0, 10);
+            }
+        });
